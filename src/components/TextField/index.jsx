ce39a016601e89a58480657f0ecbdf5c3e690aeb
spindart <spindart@proton.me>
2024-03-01 09:33:06 -0300

    refact(components/textfield): refact textfield to translate variables to english

--- src/components/TextField/index.jsx
+++ src/components/TextField/index.jsx
@@ -1,42 +1,41 @@
-import { styled } from "styled-components"
-import search from './search.png'
+import { styled } from "styled-components";
+import search from "./search.png";
 
-const ContainerEstilizado = styled.div`
-    position: relative;
-    display: inline-block;
+const ContainerStylized = styled.div`
+  position: relative;
+  display: inline-block;
 `;
 
-
-const CampoTextoEstilizado = styled.input`
-    height: 56px;
-    padding: 12px 16px;
-    border-radius: 10px;
-    border: 2px solid;
-    border-color: #C98CF1;
-    background: transparent;
-    box-sizing: border-box;
-    width: 566px;
-    color: #D9D9D9;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 20px;
-`
-
-const IconeLupa = styled.img`
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    width: 38px;
-    height: 38px;
+const TextFieldStylized = styled.input`
+  height: 56px;
+  padding: 12px 16px;
+  border-radius: 10px;
+  border: 2px solid;
+  border-color: #c98cf1;
+  background: transparent;
+  box-sizing: border-box;
+  width: 566px;
+  color: #d9d9d9;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
 `;
 
-const CampoTexto = (props) => {
-    return (
-        <ContainerEstilizado>
-            <CampoTextoEstilizado placeholder="O que você procura?" {...props} />
-            <IconeLupa src={search} alt="ícone de lupa" />
-        </ContainerEstilizado>
-    )
-}
+const SearchIcon = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 38px;
+  height: 38px;
+`;
 
-export default CampoTexto
+const TextField = (props) => {
+  return (
+    <ContainerStylized>
+      <TextFieldStylized placeholder="What do you find?" {...props} />
+      <SearchIcon src={search} alt="icon search" />
+    </ContainerStylized>
+  );
+};
+
+export default TextField;
