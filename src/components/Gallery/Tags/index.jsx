8dc87f13a03397a7d2d71f3df03075ecb4c5edb5
spindart <spindart@proton.me>
2024-03-11 12:46:15 -0300

    chore: tags

--- src/components/Gallery/Tags/index.jsx
+++ src/components/Gallery/Tags/index.jsx
@@ -1,12 +1,42 @@
+import styled from "styled-components";
 import tags from "./tags.json";
+
+const TagTitleStyle = styled.h3`
+  color: #d9d9d9;
+  font-size: 24px;
+  margin-top: 24px;
+`;
+
+const TagsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 18px;
+`;
+
+const Tag = styled.button`
+  font-size: 24px;
+  color: #ffffff;
+  background: rgba(217, 217, 217, 0.3);
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  padding: 12px;
+  box-sizing: border-box;
+  border: 2px solid transparent;
+  &:hover {
+    border-color: #c98cf1;
+  }
+`;
 
 const Tags = () => {
   return (
     <>
-      <p>Busque por tags:</p>
-      {tags.map((tag) => (
-        <button key={tag.id}>{tag.title}</button>
-      ))}
+      <TagTitleStyle>Busque por tags:</TagTitleStyle>
+      <TagsContainer>
+        {tags.map((tag) => (
+          <Tag key={tag.id}>{tag.title}</Tag>
+        ))}
+      </TagsContainer>
     </>
   );
 };
